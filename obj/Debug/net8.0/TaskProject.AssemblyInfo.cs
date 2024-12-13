a70ced0a980f9b9688760fff56726d39ff88e6e7
JasonDeckmyn <55783244+JasonDeckmyn@users.noreply.github.com>
2024-12-13 19:29:58 +0100

    Revert "Added MAUI project"
    This reverts commit 9fc59adfcb439b981886f542cccb81f56d6cd7c0.

--- obj/Debug/net8.0/TaskProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4776c7445c4cfaa79642e03b8e153d9d30d9acb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5130114f29a357ce1abe707dd73989c4429e274")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
